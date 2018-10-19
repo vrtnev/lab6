271e6f5ad90bce9c47567a98297ca2e1f5f69223
vrtnev <vrtnev@tltdev.ru>
2018-10-19 08:14:11 +0300

    Лабораторная работа №6 выполнена

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lab6';
+  sensors = ["Датчик 1", "Датчик 2"];
+  sensorActive = [false, true];
+
+  constructor() {
+    for (let i = 0; i < this.sensors.length; i++) {
+      if (Math.round(Math.random()) == 1) {
+        this.sensorActive[i] = true;
+      } else {
+        this.sensorActive[i] = false;
+      }
+    }
+  }
+
+  onDeleteSensor(i) {
+    this.sensors.splice(i,1);
+    this.sensorActive.splice(i,1);
+  }
+
+  addSensor(name: string, active) {
+    this.sensors.push(name);
+    let tmp;
+    if (active=='true')
+      tmp = true;
+    else
+      tmp = false;
+    this.sensorActive.push(tmp);
+  }
+
 }
